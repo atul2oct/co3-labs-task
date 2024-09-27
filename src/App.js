ec1c33ccd185bccd8aef0a4ddd850556c5646511
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2024-09-28 03:30:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import "./App.css"
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+// import Home from './pages/Home'
 import TapButton from './components/TapButton'
 
 const App = () => {
   return (
-    <div className='w-screen min-h-screen bg-richblack-900 flex flex-col
-    font-inter'>
+    <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter '>
       <Routes>
         {/* <Route path='/' element={<Home/>}/> */}
         <TapButton/>
